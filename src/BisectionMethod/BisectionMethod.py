954191aa72e08510fe97064496d9587b2f515d08
mrahimi74 <mr-rahimi@outlook.com>
2025-02-19 11:15:21 -0500

    add

---
+++ src/BisectionMethod/BisectionMethod.py
@@ -0,0 +1,47 @@
+def Avg(x, y):
+    return (x + y) / 2
+
+def sign(x):
+    if x > 0:
+        return 1
+    else:
+        return -1
+
+def abs(x):
+    if x >= 0:
+        return x
+    else:
+        return -x
+
+def check(a: float, b: float):
+    if a >= b:
+        raise ValueError(f"Invalid input: {a} is greater than {b}.")
+    return True
+
+
+def opps_sign(a, b, func):
+    if func(b) * func(a) < 0:
+        return True
+    else:
+        raise ValueError("f(a) and f(b) must have opposite signs")
+
+def bisection(a, b, max_iter, tol, func):
+    iter = 1
+    c_history = []
+    if opps_sign(a, b, func):
+        c = Avg(a, b)
+        c_history.append(c)
+        while abs(c - a) > tol or func(c) > tol:
+            if sign(func(c)) == sign(func(a)):
+                a = c
+            else:
+                b = c
+
+            c = Avg(a, b)
+            c_history.append(c)
+            iter += 1
+            if max_iter < iter:
+                raise ValueError("Solution did not converge as the maximum iteration reached")
+                break
+        else:
+            return c, c_history, iter
---
+++ tests/tests_BisectionMethod.py
@@ -0,0 +1,49 @@
+import numpy as np
+from bisectionmethod import BisectionMethod as BM
+import pytest
+
+def test_avg_x_y():
+  x = 10
+  y = 20
+  assert np.isclose(BM.Avg(x, y), 15)
+
+def test_sign():
+  x = 12
+  assert np.isclose(BM.sign(x), 1)
+  x = -4
+  assert np.isclose(BM.sign(x), -1)
+
+def test_abs():
+  x = 19
+  assert np.isclose(BM.abs(x), 19)
+  x = -241
+  assert np.isclose(BM.abs(x), 241)
+  
+def test_check():
+  x = 12
+  y = 17
+  assert BM.check(x,y) == True
+  x = 5
+  y = 3
+  with pytest.raises(ValueError, match = f"Invalid input: {x} is greater than {y}."):
+    BM.check(x,y)
+  
+
+f = lambda x: x**2 - 12
+
+def test_opps_sign():
+  x = 3
+  y = 5
+  assert BM.opps_sign(x, y, f) == True
+  x = 5
+  y = 3
+  with pytest.raises(ValueError, match = "f(a) and f(b) must have opposite signs"):
+    BM.opps_sign(x,y)
+
+
+g = lambda x: x ** 3 + 5
+def test_bisection():
+  p,q,r = BM.bisection(-3, 1, 200, 1e-11, g)
+  assert np.isclose(p, -1.7099759466764226)
+  with pytest.raises("Solution did not converge as the maximum iteration reached"):
+    p,q,r = BM.bisection(-3, 1, 2, 1e-11, g)
